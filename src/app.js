8d38914a38b52fbff2cb2f46ca90d3720b24f09b
Simon <simon@imac-de-simon.home>
2016-12-24 00:34:50 +0100

    app: Add import and add src scripts for index

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
+import uibootstrap from 'angular-ui-bootstrap';
+// or import accordion from 'angular-ui-bootstrap/src/accordion'; for only accordion
+
 import $ from 'jquery';
 import moment from 'moment';
 
+/* custom angular modules */
 import home from './home/home-module';
+
+// /* bootstrap less */
+// import "bootstrap-less";
+import css from 'bootstrap-less/bootstrap/bootstrap.less';
 
 angular.module('app', 
 	[
 		uirouter,
+		uibootstrap,
 		home
 	]).config(function ($urlRouterProvider, $locationProvider) {
 		$locationProvider.html5Mode(true);
