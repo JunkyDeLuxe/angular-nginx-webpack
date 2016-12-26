9887795cc246e9a937b4c1a4a68d336e435d7d7b
Simon Costa <costapasquier.simon@gmail.com>
2016-12-26 22:20:56 +0100

    webpack: Add current path from path resolve and node dirname

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,17 @@ var copyWebpackPlugin = require('copy-webpack-plugin');
 var extractTextPlugin = require('extract-text-webpack-plugin');
 var cleanWebpackPlugin = require('clean-webpack-plugin');
 
+var __currentPath = path.resolve(__dirname);
+
 module.exports = {
 	entry: {
-		app: './src/app.js',
-		vendors: [ 
-			'lodash',
-			'angular', 
-			'angular-ui-router',
-			'angular-ui-bootstrap',
-			'jquery',
-			'moment'
-		]
+		app: __currentPath + '/src/app.js',
+		vendors: ['lodash', 'angular', 'angular-ui-router', 'angular-ui-bootstrap', 'jquery', 'moment']
 	},
 	output: {
 		path: __dirname + '/dist',
 		filename: "js/app.js"
 	},
-	plugins: [
-		new cleanWebpackPlugin(['dist'], { root: path.resolve(__dirname), verbose: true, dry: false }),
-		new copyWebpackPlugin([ { from: './src/index.html' } ]),
-		new webpack.optimize.CommonsChunkPlugin("vendors", "./js/vendors.js", Infinity),
-		new extractTextPlugin("./css/styles.css")
-	],
 	module: {
 		loaders: [
 			{
@@ -47,6 +36,10 @@ module.exports = {
 				//loader: 'file-loader?name=[path][name].[ext]!extract-loader!html-loader'
 			},
 			{
+				test: /\.css$/,
+				loader: extractTextPlugin.extract("style-loader", "css-loader")
+			},
+			{
 				test: /\.less$/,
 				loader: extractTextPlugin.extract("style-loader", "css-loader!less-loader")
 			},
@@ -55,5 +48,11 @@ module.exports = {
 				loader: 'file-loader?name=/fonts/[name].[ext]'
 			}
 		]
-	}
+	},
+	plugins: [
+		new cleanWebpackPlugin(['dist'], { root: __currentPath, verbose: true, dry: false }),
+		new copyWebpackPlugin([ { from: __currentPath + '/src/index.html' } ]),
+		new webpack.optimize.CommonsChunkPlugin('vendors',  './js/vendors.js', Infinity),
+		new extractTextPlugin('./css/styles.css')
+	]
 };
