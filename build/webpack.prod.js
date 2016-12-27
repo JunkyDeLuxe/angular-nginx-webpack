6a3db34059e7b479a88235a7c20cf4b34c6e3cc7
Simon Costa <costapasquier.simon@gmail.com>
2016-12-27 22:39:17 +0100

    webpack: Prod uglify remove warnings compressor and had todo

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,10 +1,14 @@
 var webpack = require('webpack');
 var config = require('./webpack.base');
 
-// config.plugins = config.plugins.concat([
-// 	new webpack.optimize.UglifyJsPlugin({
-// 		comments: false
-// 	})
-// ]);
+// TODO Load minified libraries js files instead of minify all sources //
+config.plugins = config.plugins.concat([
+	new webpack.optimize.UglifyJsPlugin({
+		comments: false,
+		compress: {
+			warnings: false
+		}
+	})
+]);
 
 module.exports = config;
